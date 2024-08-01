05af358a009c4a1b3d260ff6ab96fc77c057751c
Lezerie Badosa <lezbadosa@gmail.com>
2024-08-02 01:49:36 +0800

    Implement the routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HomePage from "./components/home";
+import RouteComponent from "./routes/route";
+import { Provider } from "react-redux";
 import "./index.css";
+import store from "./states/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouteComponent />
+    </React.StrictMode>
+  </Provider>
 );
